ef6f93e8bce00a8131fe5196f63bae8ae1fa0b15
amazinigmech2418 <33498997+amazinigmech2418@users.noreply.github.com>
2018-04-06 17:11:08 -0400

    Update objects.js

--- objects.js
+++ objects.js
@@ -1,1 +1,15 @@
-
+Object.prototype.isObjectified = function() {return this.objectified!==undefined;}
+Object.prototype.toNumber = function() {
+  if(this.isObjectified) {
+  if(this.type==="number"){
+  return this.value;
+  }
+  }
+}
+Object.prototype.toStringObjectified = function() {
+  if(this.isObjectified) {
+  if(this.type==="number"){
+  return this.value.toString();
+  } else {return this.value;}
+  }
+}
