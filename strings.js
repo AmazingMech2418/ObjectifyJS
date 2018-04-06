51f955cd3b9c45113589f89cb89d93294fb81c35
amazinigmech2418 <33498997+amazinigmech2418@users.noreply.github.com>
2018-04-06 13:05:07 -0400

    Update strings.js

--- strings.js
+++ strings.js
@@ -1,1 +1,17 @@
-
+String.prototype.splitBrackets=function(open,close) {
+var a = [""];
+var brackets = 0;
+for(var i=0; i<this.length; i++) {
+  if (this[i]===open) {
+  brackets += 1;
+    if(brackets === 1){
+    a.push("");
+    } else {a[a.length-1] += this[i];}
+  } else if (this[i]===close) {
+  brackets -= 1;if(brackets!==0){a[a.length-1] += this[i];}
+  } else {
+  a[a.length-1] += this[i];
+  }
+}
+  return a;
+}
