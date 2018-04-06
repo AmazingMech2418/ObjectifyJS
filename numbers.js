176a5de9638ea7cfb83d16e35c655628ae239e12
amazinigmech2418 <33498997+amazinigmech2418@users.noreply.github.com>
2018-04-06 12:21:47 -0400

    Update numbers.js

--- numbers.js
+++ numbers.js
@@ -1,1 +1,21 @@
+// Examples:
+// toObject(): var a = 31; a = a.toObject();
+// sin(): var a = 31; a = a.sin();
+// cos(): var a = 31; a = a.cos();
+// tan(): var a = 31; a = a.tan();
+// abs(): var a = 31; a = a.abs();
+// toPowerOf(): var a = 2; a = a.toPower(3);
 
+Number.prototype.toObject = function() {
+var a = new Object();
+a.type = "number";
+a.objectified = true;
+a.value = this.valueOf();
+return a;
+}
+Number.prototype.sin = function() {Math.sin(this.valueOf);}
+Number.prototype.cos = function() {Math.cos(this.valueOf);}
+Number.prototype.tan = function() {Math.tan(this.valueOf);}
+Number.prototype.abs = function() {Math.abs(this.valueOf);}
+Number.prototype.toPowerOf = function(exponent) {Math.pow(this.valueOf,exponent);}
+Number.toText = function() {String.charcode(this.valueOf);}
